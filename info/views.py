88f949050021c0e86e4277054e1e6eb836116a4a
Shen Zhuoran <cmsflash99@gmail.com>
2019-03-05 13:34:34 +0800

    Add testing index page

--- info/views.py
+++ info/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 from .models import Staff
 
 
+DEFAULT_STAFF_ID = '00001216'
+
+
+def index(request):
+    redirection = redirect(DEFAULT_STAFF_ID)
+    return redirection
+
+
 def info(request, id_):
     staff = Staff.objects.get(pk=id_)
     return HttpResponse(f'{staff.to_json()}')
