d6b316282be9edcf54447ca115fbd663f73453fa
Shen Zhuoran <cmsflash99@gmail.com>
2019-03-05 16:56:51 +0800

    Add a catch-all term in main url parser

--- hrserver/urls.py
+++ hrserver/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 
 urlpatterns = [
     path('', include('index.urls')),
     path('info/', include('info.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'.*', include('index.urls')),
 ]
