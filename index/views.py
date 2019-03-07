a6422f0b09673a625206b6ab4c0203c692eefa5a
Shen Zhuoran <cmsflash99@gmail.com>
2019-03-07 22:19:56 +0800

    Fix error response

--- index/views.py
+++ index/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    url = request.build_absolute_uri() 
+    url = request.build_absolute_uri()
+    base_url = '/'.join(url.split('/')[:3] + [''])
     response = HttpResponse(
-        f'Please make an HTTP GET request to {url}info/&ltstaff_id&gt'
+        f'Please make an HTTP GET request to {base_url}info/&ltstaff_id&gt'
     )
     return response
