7b752df410db32749f34009290b846444a24e6ef
Shen Zhuoran <cmsflash99@gmail.com>
2019-03-07 22:19:42 +0800

    Fix info request url

--- hrserver/urls.py
+++ hrserver/urls.py
@@ -3,8 +3,8 @@ from django.urls import include, path, re_path
 
 
 urlpatterns = [
-    path('', include('index.urls')),
     path('info/', include('info.urls')),
     path('admin/', admin.site.urls),
+    path('', include('index.urls')),
     re_path(r'.*', include('index.urls')),
 ]
--- info/urls.py
+++ info/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('<str:id_>/', views.info),
+    path('<str:id_>', views.info),
 ]
