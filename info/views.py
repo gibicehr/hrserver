f7af3521ebf11643790f574520a832bc3a135430
Shen Zhuoran <cmsflash99@gmail.com>
2019-03-11 18:58:43 +0800

    Add logic for nonexistent ids

--- info/views.py
+++ info/views.py
@@ -7,6 +7,9 @@ DEFAULT_STAFF_ID = '00001216'
 
 
 def info(request, id_):
-    staff = Staff.objects.get(pk=id_)
-    response = HttpResponse(f'{staff.to_json()}')
+    try:
+        staff = Staff.objects.get(pk=id_)
+        response = HttpResponse(f'{staff.to_json()}')
+    except Staff.DoesNotExist:
+        response = HttpResponse('Invalid staff ID')
     return response
